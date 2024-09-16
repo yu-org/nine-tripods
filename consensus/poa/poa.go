d7684b5059b54172098bdd1e759d29b341a7ebb5
Lawliet-Chan <1576710154@qq.com>
2024-09-16 22:30:39 +0800

    upgrade yu

--- consensus/poa/poa.go
+++ consensus/poa/poa.go
@@ -268,7 +268,7 @@ func (h *Poa) FinalizeBlock(block *types.Block) {
 	if h.cfg.PrettyLog {
 		log.DoubleLineConsole.Info(fmt.Sprintf("finalize block, height=%d, hash=%s", block.Height, block.Hash.String()))
 	}
-	h.Chain.Finalize(block.Hash)
+	h.Chain.Finalize(block)
 }
 
 func (h *Poa) CompeteLeader(blockHeight common.BlockNum) common.Address {
